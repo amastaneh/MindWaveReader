d3b3c17b1b4c5f3fca97f102b449d364ddda5433
amastaneh@gmail.com <amastaneh@gmail.com>
2017-04-09 17:35:42 +0430

    v1.0

--- MindWaveReaderWPF/Properties/AssemblyInfo.cs
+++ MindWaveReaderWPF/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MindWaveReaderWPF")]
+[assembly: AssemblyTitle("MindWave Reader")]
 [assembly: AssemblyDescription("قرائتگر امواج مغزی تک کاناله")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WebDesignIran")]
-[assembly: AssemblyProduct("MindWaveReaderWPF")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("MindWave Reader")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("WebDesignIran")]
 [assembly: AssemblyCulture("")]
 
